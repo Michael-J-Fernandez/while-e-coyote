d6ed53964479840c5a827ed0105dc971ca874612
Michael-J-Fernandez <michael.fernandez@codeimmersives.com>
2022-09-21 18:32:45 -0400

    Adjust formatting for readability.

--- fibonacci.js
+++ fibonacci.js
@@ -2,16 +2,18 @@ const prompt = require('prompt-sync')({ sigint: true });
 
 let n = Number(prompt("Enter a number: "));
 
-check = 0;    // checks for equality with n
-let f0 = 0;   // first initial value in sequence
-let f1 = 1;   // second initial value in sequence
-let fib = 0;  // sum of two previous numbers
+let f0 = 0;     // first initial value in sequence
+let f1 = 1;     // second initial value in sequence
+let fib = 0;    // sum of two previous numbers
+let check = 0;  // checks for equality with n
 
 while (check < n) {
-    console.log (fib)
+    console.log(fib)
+    
     f0 = f1;
     f1 = fib;
     fib = f0 + f1;
 
     check++;
 }
+
--- make-divisible.js
+++ make-divisible.js
@@ -3,12 +3,10 @@ const prompt = require('prompt-sync')({ sigint: true });
 let x = Number(prompt("Enter x; "));
 let y = Number(prompt("Enter y; "));
 
-
-
 if (isNaN(x) || isNaN(y)) {  // returns error if NaN to avoid infinite loop
     console.log("Error - One or more inputs is not a number.")
 } else {
-    while (x % y != 0) {
+    while (x % y !== 0) {
         console.log(x);
         x++
     }
